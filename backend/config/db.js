2c731c71247d02281a344f6a2e56d4fc5b6e9e66
Jenanouar55 <jenanouar1234@gmail.com>
2025-05-14 05:17:33 +0100

    pushed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,22 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-<<<<<<< HEAD
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-=======
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ Error: ${error.message}`);
->>>>>>> 6ba3e94c1d6e0527e84738647d2976ce7c797a94
-    process.exit(1);
+    console.error(`❌ MongoDB connection error: ${error.message}`);
+    process.exit(1); 
   }
 };
 
