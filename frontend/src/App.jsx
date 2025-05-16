041ab19c32dff6de81e4db29692370aed377bc75
Jenanouar55 <jenanouar1234@gmail.com>
2025-05-16 16:51:50 +0100

    pushed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Existing imports
 import LoginForm from './components/Signup/LoginPage';
 import SignupForm from './components/Signup/SignupForm';
 import LandingPage from './Pages/LandingPage';
+
+// Dashboard page 
+import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/*routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
+
+        {/* Dashboardroutes */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard/contact" element={<Contact />} />
+        <Route path="/dashboard/history" element={<History />} />
+        <Route path="/dashboard/profile" element={<Profile />} />
+        <Route path="/dashboard/settings" element={<Settings />} /> */}
       </Routes>
     </Router>
   );
