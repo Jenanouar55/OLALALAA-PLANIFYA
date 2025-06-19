c4543491510e31071c3a3071e9371a8c14f64f8e
Jenanouar55 <jenanouar1234@gmail.com>
2025-06-19 04:53:50 +0100

    fixing merges

--- frontend/src/components/Signup/LoginPage.jsx
+++ frontend/src/components/Signup/LoginPage.jsx
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({ email: '', password: '' });
