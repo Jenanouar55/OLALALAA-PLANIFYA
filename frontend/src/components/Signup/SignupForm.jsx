835069891d5f5f7e732630dd39b487f3c6859d07
Jenanouar55 <jenanouar1234@gmail.com>
2025-06-19 04:51:31 +0100

    fixing merge

--- frontend/src/components/Signup/SignupForm.jsx
+++ frontend/src/components/Signup/SignupForm.jsx
@@ -2,9 +2,6 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Link, useNavigate } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const SignupForm = () => {
   const [formData, setFormData] = useState({
